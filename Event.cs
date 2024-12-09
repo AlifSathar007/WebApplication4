4af90a455cb98efb24ff30e39d312a62f50d7096
Alif Sathar <153131373+AlifSathar007@users.noreply.github.com>
2024-12-09 19:24:51 +0000

    Add files via upload

---
+++ Event.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EventAndContentApp.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+    }
+}
